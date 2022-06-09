6dd78980224baa20ece2742e504b05790b234a16
minsu Lee <dlalstn5789@gmail.com>
2022-06-09 23:50:49 +0900

    degree의 경우 5, 7, 9 전부 한 main에서 수행하고 결과 내는 부분 수정

--- B-Tree/btree_implementation/btree/BTree.java
+++ B-Tree/btree_implementation/btree/BTree.java
@@ -7,22 +7,27 @@ import java.util.*;
 
 public class BTree {
 	public static void main(String[] args) {
-		//과제 조건: 시간 측정
-		long startTime = System.nanoTime();
-		int degree = 5;
-		//int degree = 7;
-		//int degree = 9;
+	
+		int degree5 = 5;
+		int degree7 = 7;
+		int degree9 = 9;
 		
-		B_tree t = new B_tree(degree); // B-Tree with minimum degree 5, 7, 9 위 주석부분에서 선택 (입력받기 귀찮....
-        System.out.println(String.format("B-Tree minimum degree is %d", degree));
-
-
 		int [] rand_array = new int[1000];
-		//랜덤 숫자 생성
-		Random rand = new Random();
-		int insert_number;
-		int count_i = 0;
+		//랜덤 숫자 생성 -seed값 고정 실행시간 분석을 위해
+		Random rand = new Random(10);
 		
+		int insert_number = 0;
+		int count_i = 0;
+		int remove_index = 0;
+		int count_r = 0;
+		
+		B_tree t5 = new B_tree(degree5);
+		B_tree t7 = new B_tree(degree7);
+		B_tree t9 = new B_tree(degree9);// B-Tree with minimum degree 5, 7, 9 위 주석부분에서 선택 (입력받기 귀찮....
+        System.out.println(String.format("B-Tree minimum degree is %d", degree5));
+        
+        //과제 조건: 시간 측정
+      	long startTime5 = System.nanoTime();
 		//과제 조건: 0~9999까지의 수중 랜덤으로 배열에 넣기 -> remove시 사용 
 		//입력자료는 중복될 수 없다.
 		while(count_i<1000) {
@@ -30,27 +35,24 @@ public class BTree {
 			if(!Arrays.asList(rand_array).contains(insert_number)) {
 				rand_array[count_i] = insert_number;
 				//배열에 들어가는 값을 트리의 insert값으로 넣기
-				t.insert(rand_array[count_i]);
+				t5.insert(rand_array[count_i]);
 				count_i++;
 			}
 			else
 				continue;
 		}
-        System.out.println();
+        //System.out.println();
 
-		System.out.println("Traversal of tree after insert");
-        t.traverse();
-        System.out.println();
-        
-		int remove_index = 0;
-		int count_r = 0;
+		//System.out.println("Traversal of tree after insert");
+        //t.traverse();
+        //System.out.println();
 		
 		//과제 조건: 500개 항목 지우기
 		while(count_r < 500) {
 			remove_index = rand.nextInt(1000);
 			//500개의 항목를 삭제해야하므로, 겹치는 랜덤 수가 없도록 조건문 실행
 			if(rand_array[remove_index] != -1) {
-				t.remove(rand_array[remove_index]);
+				t5.remove(rand_array[remove_index]);
 				rand_array[remove_index] = -1;
 				count_r++;
 			}
@@ -58,15 +60,120 @@ public class BTree {
 				continue;
 			}
 		}
-        System.out.println();
+		//System.out.println();
 
-		System.out.println("Traversal of tree after remove");
-        t.traverse();
-        System.out.println();
+		//System.out.println("Traversal of tree after remove");
+        //t.traverse();
+        //System.out.println();
+        
+        long endTime5 = System.nanoTime();
+        System.out.println(String.format("B-Tree insert, remove run time is %d ns", endTime5-startTime5));
+        
         
         System.out.println();
-        long endTime = System.nanoTime();
-        System.out.println(String.format("B-Tree insert, remove run time is %d ns", endTime-startTime));
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        //degree 7인 경우
+        insert_number = 0;
+		count_i = 0;
+		remove_index = 0;
+		count_r = 0;
+        System.out.println(String.format("B-Tree minimum degree is %d", degree7));
+        
+        //과제 조건: 시간 측정
+      	long startTime7 = System.nanoTime();
+		//과제 조건: 0~9999까지의 수중 랜덤으로 배열에 넣기 -> remove시 사용 
+		//입력자료는 중복될 수 없다.
+		while(count_i<1000) {
+			insert_number = rand.nextInt(10000);
+			if(!Arrays.asList(rand_array).contains(insert_number)) {
+				rand_array[count_i] = insert_number;
+				//배열에 들어가는 값을 트리의 insert값으로 넣기
+				t7.insert(rand_array[count_i]);
+				count_i++;
+			}
+			else
+				continue;
+		}
+        //System.out.println();
+
+		//System.out.println("Traversal of tree after insert");
+        //t.traverse();
+        //System.out.println();
+		
+		//과제 조건: 500개 항목 지우기
+		while(count_r < 500) {
+			remove_index = rand.nextInt(1000);
+			//500개의 항목를 삭제해야하므로, 겹치는 랜덤 수가 없도록 조건문 실행
+			if(rand_array[remove_index] != -1) {
+				t7.remove(rand_array[remove_index]);
+				rand_array[remove_index] = -1;
+				count_r++;
+			}
+			else {
+				continue;
+			}
+		}
+		//System.out.println();
+
+		//System.out.println("Traversal of tree after remove");
+        //t.traverse();
+        //System.out.println();
+        
+        long endTime7 = System.nanoTime();
+        System.out.println(String.format("B-Tree insert, remove run time is %d ns", endTime7-startTime7));
+        
+        
+        System.out.println();
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////
+        //degree 9인 경우
+        insert_number = 0;
+		count_i = 0;
+		remove_index = 0;
+		count_r = 0;
+        System.out.println(String.format("B-Tree minimum degree is %d", degree9));
+        
+        //과제 조건: 시간 측정
+      	long startTime9 = System.nanoTime();
+		//과제 조건: 0~9999까지의 수중 랜덤으로 배열에 넣기 -> remove시 사용 
+		//입력자료는 중복될 수 없다.
+		while(count_i<1000) {
+			insert_number = rand.nextInt(10000);
+			if(!Arrays.asList(rand_array).contains(insert_number)) {
+				rand_array[count_i] = insert_number;
+				//배열에 들어가는 값을 트리의 insert값으로 넣기
+				t9.insert(rand_array[count_i]);
+				count_i++;
+			}
+			else
+				continue;
+		}
+        //System.out.println();
+
+		//System.out.println("Traversal of tree after insert");
+        //t.traverse();
+        //System.out.println();
+		
+		//과제 조건: 500개 항목 지우기
+		while(count_r < 500) {
+			remove_index = rand.nextInt(1000);
+			//500개의 항목를 삭제해야하므로, 겹치는 랜덤 수가 없도록 조건문 실행
+			if(rand_array[remove_index] != -1) {
+				t9.remove(rand_array[remove_index]);
+				rand_array[remove_index] = -1;
+				count_r++;
+			}
+			else {
+				continue;
+			}
+		}
+		//System.out.println();
+
+		//System.out.println("Traversal of tree after remove");
+        //t.traverse();
+        //System.out.println();
+        
+        long endTime9 = System.nanoTime();
+        System.out.println(String.format("B-Tree insert, remove run time is %d ns", endTime9-startTime9));
 
 	}
 
